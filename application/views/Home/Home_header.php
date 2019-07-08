60cec192307a80423403ba25cb60f131d72c48e0
Mahdi Mashal <mhdml1379@gmail.com>
2019-07-08 14:30:33 +0430

    Initial commit

---
+++ application/views/Home/Home_header.php
@@ -0,0 +1,15 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');  ?>
+<!DOCTYPE html>
+<html lang="en"> <!--TODO-->
+<head>
+    <title>Blogma</title>
+    <meta charset="utf-8" />
+    <meta name="description" content="Create your free Weblog" />
+    <meta name="keywords" content="blog,post" />
+    <meta name="author" content="Mahdi Mashal" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</head>
+<body>
+
+</body>
+</html>
